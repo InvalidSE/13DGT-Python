cdefbac572fea74b2549c44c5ff13a1e38cd2d2b
InvalidSE <taine.reader@gmail.com>
2023-04-06 21:19:59 +1200

    updates

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ topics = {
     "Cartoon & Animations": 32
 }
 difficulties = ["easy", "medium", "hard"]
+max_questions = 25
+max_users = 10
 
 # =================== IMPORTS ===================
 from random import randint, shuffle
@@ -105,19 +107,135 @@ class User:
 
 # This class is used to print colored text
 # I got it from https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-python
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+class colours:
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 
 # =================== FUNCTIONS ===================
+
+# This function verifies that the input is a number between the min and max
+def verify_input(input_num, min, max):
+    while True:
+        try:
+            input_num = int(input_num)
+            if input_num < min or input_num > max:
+                raise ValueError
+            break
+        except ValueError:
+            input_num = input(f"Please enter a number between {min} and {max}: ")
+    return input_num
+
+
+
+# This function makes a box around the text
+def box_print(text):
+    terminal_size = os.get_terminal_size()
+    output = "╔" + "═"*(terminal_size.columns-2) + "╗\n"
+    
+    if("\n" in text):
+        lines = text.split("\n")
+    else:
+        lines = [text]
+
+    for line in lines:
+        words = line.split(" ")
+        current_line = ""
+        for word in words:
+            if len(current_line + word) > terminal_size.columns-4:
+                output += "║ " + current_line + " "*(terminal_size.columns-4-len(current_line)) + " ║\n"
+                current_line = ""
+            current_line += word 
+            if(current_line != ""):
+                if(len(current_line) < terminal_size.columns-4):
+                    current_line += " "
+        output += "║ " + current_line + " "*(terminal_size.columns-4-len(current_line)) + " ║\n"
+        
+    output += "╚" + "═"*(terminal_size.columns-2) + "╝"
+    print(output)
+
+
+
+# Welcome message
+def welcome():
+    os.system(clear_screen)
+    # print welcome text if the console is big enough
+    terminal_size = os.get_terminal_size()
+    if terminal_size.columns >= 44:
+        print(welcome_text)
+
+     # ask the user if they want to play a quiz or view previous quizzes
+    print("\nWelcome to the quiz!\nSelect an option:\n")
+    print("1. Play a quiz")
+    print("2. View previous quiz")
+    print("3. Exit")
+
+    # get the user's choice
+    return verify_input(input("\nEnter the number of your choice: "), 1, 3)
+
+
+
+# This is the main function
+def main():
+
+    # get the options
+    topic, difficulty, amount, users = get_options()
+
+    # get the questions and put them in a list of Question objects
+    questions = get_questions(topics[topic], difficulties[difficulty-1], amount)
+
+    # start the game
+    print(f"Starting the game with {len(users)} players...")
+    sleep(1)
+
+    # start the quiz
+    for i, user in enumerate(users):
+        quiz(questions=questions, player_number=i, users=users)
+
+    # print the final results
+    final_results(users, questions)
+
+    # store the quiz
+    store_quiz(questions, users)
+
+
+
+# This function gets all the options from the user and returns them 
+def get_options():
+
+    # get the topic
+    print("\nPlease select a topic:")
+    for i, topic in enumerate(topics):
+        print(f"{i+1}. {topic.title()}")
+    topic = verify_input(input("Enter the number of the topic: "), 1, len(topics))
+    topic = list(topics.keys())[topic-1]
+
+    # get the difficulty
+    print("\nPlease select a difficulty:")
+    for i, difficulty in enumerate(difficulties):
+        print(f"{i+1}. {difficulty.title()}")
+    difficulty = verify_input(input("Enter the number of the difficulty: "), 1, len(difficulties))
+
+    # get the amount of questions
+    amount = verify_input(input("\nHow many questions do you want? "), 1, max_questions)
+
+    # get the amount of players
+    players = verify_input(input("\nHow many players are there? "), 1, max_users)
+
+    #users is a list of User objects
+    users = []
+
+    # get the players' names
+    for i in range(players):
+        name = input(f"\nWhat is the name of player {i+1}? ")
+        while name == "":
+            name = input(f"What is the name of player {i+1}? ")
+        users.append(User(name))
+            
+    return topic, difficulty, amount, users
+
 
 
 # This function gets the questions from the API and returns them as a list of Question objects
@@ -169,80 +287,6 @@ def get_questions(topic, difficulty, amount):
     return questions
 
 
-# This function verifies that the input is a number between the min and max
-def verify_input(input_num, min, max):
-    while True:
-        try:
-            input_num = int(input_num)
-            if input_num < min or input_num > max:
-                raise ValueError
-            break
-        except ValueError:
-            input_num = input(f"Please enter a number between {min} and {max}: ")
-    return input_num
-
-
-# This function makes a box around the text
-def box_print(text):
-    terminal_size = os.get_terminal_size()
-    output = "╔" + "═"*(terminal_size.columns-2) + "╗\n"
-    
-    if("\n" in text):
-        lines = text.split("\n")
-    else:
-        lines = [text]
-
-    for line in lines:
-        words = line.split(" ")
-        current_line = ""
-        for word in words:
-            if len(current_line + word) > terminal_size.columns-4:
-                output += "║ " + current_line + " "*(terminal_size.columns-4-len(current_line)) + " ║\n"
-                current_line = ""
-            current_line += word 
-            if(current_line != ""):
-                if(len(current_line) < terminal_size.columns-4):
-                    current_line += " "
-        output += "║ " + current_line + " "*(terminal_size.columns-4-len(current_line)) + " ║\n"
-        
-    output += "╚" + "═"*(terminal_size.columns-2) + "╝"
-    print(output)
-
-
-# This function gets all the options from the user and returns them 
-def get_options():
-
-    # get the topic
-    print("\nPlease select a topic:")
-    for i, topic in enumerate(topics):
-        print(f"{i+1}. {topic.title()}")
-    topic = verify_input(input("Enter the number of the topic: "), 1, len(topics))
-    topic = list(topics.keys())[topic-1]
-
-    # get the difficulty
-    print("\nPlease select a difficulty:")
-    for i, difficulty in enumerate(difficulties):
-        print(f"{i+1}. {difficulty.title()}")
-    difficulty = verify_input(input("Enter the number of the difficulty: "), 1, len(difficulties))
-
-    # get the amount of questions
-    amount = verify_input(input("\nHow many questions do you want? "), 1, 25)
-
-    # get the amount of players
-    players = verify_input(input("\nHow many players are there? "), 1, 8)
-
-    #users is a list of User objects
-    users = []
-
-    # get the players' names
-    for i in range(players):
-        name = input(f"\nWhat is the name of player {i+1}? ")
-        while name == "":
-            name = input(f"What is the name of player {i+1}? ")
-        users.append(User(name))
-            
-    return topic, difficulty, amount, users
-
 
 # The main quiz function
 def quiz(questions, player_number, users):
@@ -276,14 +320,15 @@ def quiz(questions, player_number, users):
 
         if correct_answer_position+1 == answer:
             users[player_number].add_score(1)
-            print(bcolors.OKGREEN + "\nCorrect!" + bcolors.ENDC)
+            print(colours.GREEN + "\nCorrect!" + colours.ENDC)
         else:
-            print(bcolors.FAIL + "\nIncorrect!" + f"\nThe correct answer was {question.correct_answer}." + bcolors.ENDC)
+            print(colours.RED + "\nIncorrect!" + f"\nThe correct answer was {question.correct_answer}." + colours.ENDC)
 
 
         users[player_number].add_answer({"question": question.question, "correct_answer": question.correct_answer, "user_answer": answers_list[answer-1], "correct": correct_answer_position+1 == answer})
         # print(question.correct_answer, str(answers_list))
         input("\nPress enter to continue...")
+
 
 
 # This function prints the final results
@@ -297,13 +342,13 @@ def final_results(users, questions):
         answer_string = "["
         for j, question in enumerate(user.answers):
             if question["correct"]:
-                answer_string += bcolors.OKGREEN + "•" + bcolors.ENDC
+                answer_string += colours.GREEN + "•" + colours.ENDC
             else:
-                answer_string += bcolors.FAIL + "•" + bcolors.ENDC
+                answer_string += colours.RED + "•" + colours.ENDC
             if j == len(user.answers)-1:
                 answer_string += "]"
 
-        leaderboard_string = (f"{i+1}. {user.name}: {user.score}/{len(questions)} " + bcolors.ENDC)
+        leaderboard_string = (f"{i+1}. {user.name}: {user.score}/{len(questions)} " + colours.ENDC)
         if(len(leaderboard_string) + len(questions) + 3 <= os.get_terminal_size().columns):
             leaderboard_string += answer_string
 
@@ -311,26 +356,7 @@ def final_results(users, questions):
 
 
     input("\nPress enter to continue...")
-    
 
-
-# Welcome message
-def welcome():
-    os.system(clear_screen)
-    # print welcome text if the console is big enough
-    terminal_size = os.get_terminal_size()
-    if terminal_size.columns >= 44:
-        print(welcome_text)
-
-     # ask the user if they want to play a quiz or view previous quizzes
-    print("\nWelcome to the quiz!\nSelect an option:\n")
-    print("1. Play a quiz")
-    print("2. View previous quiz")
-    print("3. Exit")
-
-    # get the user's choice
-    choice = verify_input(input("\nEnter the number of your choice: "), 1, 3)
-    return choice
     
 
 # Store previous quiz in a json file
@@ -351,6 +377,7 @@ def store_quiz(questions, users):
     # write the dictionary to a json file
     with open("quiz.json", "w") as file:
         json.dump(quiz_dict, file, indent=4)
+
 
 
 # View previous quiz
@@ -392,38 +419,15 @@ def view_previous_quiz():
 
             box_print(f"Name: {user['name']}\nScore: {correct_answers}/{total_questions}\nPress enter view next question.")
 
-            print(f"{bcolors.BOLD}Question {j+1}: {answer['question']}{bcolors.ENDC}")
+            print(f"{colours.BOLD}Question {j+1}: {answer['question']}{colours.ENDC}")
             if(answer["correct"]):
-                print(bcolors.OKGREEN + "User answer: " + str({answer['user_answer']})[2:-2] + bcolors.ENDC)
+                print(colours.GREEN + "User answer: " + str({answer['user_answer']})[2:-2] + colours.ENDC)
             else:
-                print(bcolors.FAIL + "User answer: " + str({answer['user_answer']})[2:-2] + bcolors.ENDC)
-                print(f"{bcolors.OKCYAN}Correct answer: {answer['correct_answer']}{bcolors.ENDC}")
+                print(colours.RED + "User answer: " + str({answer['user_answer']})[2:-2] + colours.ENDC)
+                print(f"{colours.CYAN}Correct answer: {answer['correct_answer']}{colours.ENDC}")
             
             input("\n")
 
-
-# This is the main function
-def main():
-
-    # get the options
-    topic, difficulty, amount, users = get_options()
-
-    # get the questions and put them in a list of Question objects
-    questions = get_questions(topics[topic], difficulties[difficulty-1], amount)
-
-    # start the game
-    print(f"Starting the game with {len(users)} players...")
-    sleep(1)
-
-    # start the quiz
-    for i, user in enumerate(users):
-        quiz(questions=questions, player_number=i, users=users)
-
-    # print the final results
-    final_results(users, questions)
-
-    # store the quiz
-    store_quiz(questions, users)
 
 
 # =================== MAIN ===================
